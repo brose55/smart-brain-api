738b75c5d66e76fa3414c72ae23d3c9051939678
Brandon Rose <brandonrose805@gmail.com>
2018-09-17 21:42:38 -0700

    update database

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'mango55',
+    host : 'postgresql-adjacent-62073',
+    user : 'blaze',
+    password : '',
     database : 'smart-brain-db'
   }
 });
@@ -30,7 +30,7 @@ app.post('/signin', (req, res) => { signIn.handleSignIn(req, res, db, bcrypt) })
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
+app.post('/imageUrl', (req, res) => { image.handleApiCall(req, res) })
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`app is running on port ${process.env.PORT}`);
