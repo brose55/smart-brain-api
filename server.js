7ff6e37e308990241e48c41313f18caac6acb52c
Brandon Rose <brandonrose805@gmail.com>
2020-11-15 15:20:44 -0800

    fix typo in server.js

--- server.js
+++ server.js
@@ -30,6 +30,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageUrl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(8000, () => {
-  console.log(`app is running on port 8000`);
+app.listen(process.env.DATABASE_URL || 8000, () => {
+  console.log(`app is running on port ${process.env.DATABASE_URL}`);
 })
