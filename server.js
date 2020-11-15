56cce89b07b8425c38520ba378e63212e2d65ed7
Brandon Rose <brandonrose805@gmail.com>
2020-11-15 15:19:36 -0800

    fix typo in server.js

--- server.js
+++ server.js
@@ -23,13 +23,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send(database.users) })
+app.get('/', (req, res) => { res.send(db.users) })
 app.post('/signin', (req, res) => { signIn.handleSignIn(req, res, db, bcrypt) })
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageUrl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`app is running on port ${process.env.PORT}`);
+app.listen(8000, () => {
+  console.log(`app is running on port 8000`);
 })
